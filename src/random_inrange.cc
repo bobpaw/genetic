9747a09c7bac1d532332ba373796924c7756d77b
Aiden Woodruff <aiden.woodruff@gmail.com>
2019-01-25 20:30:41 -0600

    Print 2000 numbers for fakerandom test
    Signed-off-by: Aiden Woodruff <aiden.woodruff@gmail.com>

--- src/random_inrange.cc
+++ src/random_inrange.cc
@@ -3,7 +3,7 @@
 
 int main () {
 	fakerandom::rand_device random(0, 100);
-	for (int i = 0; i < 100; ++i) {
+	for (int i = 0; i < 2000; ++i) {
 		std::cout << random() << '\n';
 	}
 	std::cout << std::flush;
